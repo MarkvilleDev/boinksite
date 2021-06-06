0bca95b4c0c58e8c9f910e8b16ac11f0f1ebf7ba
Sarina Li <sarinajin.li@gmail.com>
2021-06-06 17:58:07 -0400

    fuck u js

--- bounce.js
+++ bounce.js
@@ -1,5 +1,5 @@
 const FPS = 60;
-var bs = 5;
+var bs = 10;
 var bx, by;
 
 var pxpfx, pxpfy;
@@ -13,8 +13,8 @@ setInterval(update, 1000 / FPS);
 bx = canvas.width /2;
 by = canvas.height /2;
 
-pxpfx = Math.floor(Math.random()*50+10) / FPS;
-pxpfy = Math.floor(Math.random()*50+10) / FPS;
+pxpfx = Math.floor(Math.random()*500+10) / FPS;
+pxpfy = Math.floor(Math.random()*500+10) / FPS;
 
 if (Math.floor(Math.random() *2) == 0) {
     pxpfx = -pxpfx;
@@ -22,17 +22,18 @@ if (Math.floor(Math.random() *2) == 0) {
 if (Math.floor(Math.random() *2) == 0) {
     pxpfy = -pxpfy;
 }
-
 function update() {
+    context.fillStyle = "#2f4672";
     bx += pxpfx;
     by += pxpfy;
-    context.fillStyle = "#2f4672";
+    context.canvas.width = document.documentElement.clientWidth;
+    context.canvas.height = document.documentElement.clientHeight;
     context.fillRect(0, 0, canvas.width, canvas.height);
     context.beginPath();
     wd = window.innerWidth;
-    ht = window.innerHeight
+    ht = window.innerHeight;
     context.fill();
-    context.lineWidth = 5;
+    context.lineWidth = 7;
     context.strokeStyle = "white";
     context.arc(bx - bs/2,by - bs/2,bs/2, 0,2*Math.PI);
     context.stroke();
@@ -46,12 +47,12 @@ function update() {
     if (by - bs/2 < 0 && pxpfy<0) {
         pxpfy = -pxpfy;
     }
-    if (by + bs/2 > canvas.height && pxpfy>0) {
+    if (by + bs/2 > ht && pxpfy>0) {
         pxpfy = -pxpfx;
     }
 
 }
-function init() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
+// function init() {
+//     canvas.width = window.innerWidth;
+//     canvas.height = window.innerHeight;
+// }
